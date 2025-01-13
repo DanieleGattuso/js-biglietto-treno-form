825c3ac16b92d5663a52fbc0a54731b1178fcb07
daniele.gattuso <d.gattuso09@gmail.com>
2025-01-13 22:50:06 +0100

    complete excercize

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 
 const nameField = document.getElementById('nome_biglietto');
 const kmField = document.getElementById('km_biglietto');
-const ageField = document.getElementById('age_biglietto');
+const ageField = document.getElementById('age');
 
 
 
@@ -16,21 +16,49 @@ const trainPosition = document.getElementById('carriage');
 const ticketCode = document.getElementById('ticket-code');
 const ticketPrice = document.getElementById('ticket-price');
 const container = document.getElementById('micro');
+const button = document.getElementById('green_button')
 
 // Scrivendo nel form prendiamo i valori dell'input e li scriviamo nei determinati blocchi
 
-container.addEventListener('input' , (event) => {
+button.addEventListener('click' , (event) => {
 
 
     // Blocchiamo l'invio del form
-    // event.preventDefault();
+    event.preventDefault();
 
     // Stampiamo il codice
 
     passengerName.textContent = nameField.value.trim();
 
+      
+    let pricePerKm = 0.21;
+    let discount = 0;
+    
 
+    if(ageField.value === "minorenne"){
 
+        discount = 0.2;
+        ticketType.textContent = "Biglietto scontato (Minorenne)"
+
+    }else if(ageField.value === "over65"){
+        ticketType.textContent = "Biglietto scontato (Over 65)"
+        discount = 0.4;
+    }else{
+        ticketType.textContent = 'Biglietto Standard';
+    }
+
+    const totalPrice = (kmField.value * pricePerKm * (1 - discount)).toFixed(2)
+
+    
+
+    ticketPrice.textContent = `${totalPrice} €`;
+
+    // Generare numeri causuali per riempire spazi vuoti
+    const randomCode = Math.floor(Math.random() * 100000) + 1; // Numero casuale tra 1 e 100000
+    const randomPositionCode =Math.floor(Math.random() * 10) + 1
+    ticketCode.textContent = `CP-${randomCode}`; // Stampiamo il codice nel formato "CP-XXXXX"
+    trainPosition.textContent = randomPositionCode; // Stampiamo il codice nel formato XXXXX
+    
 
 })
 
