f809ac9d3b6d18399477fce050289f723551b09f
daniele.gattuso <d.gattuso09@gmail.com>
2025-01-13 17:09:08 +0100

    30% complete

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
 
-// Definiamo tutti gli elementi inseriti
+// Definiamo tutti gli elementi inseriti in input
 
-const nameField = document.getElementById('nome_biglietto')
-console.log(nameField);
+const nameField = document.getElementById('nome_biglietto');
+const kmField = document.getElementById('km_biglietto');
+const ageField = document.getElementById('age_biglietto');
+
+// Definiamo tutti gli elementi della pagina in output
+
+const name = document.getElementById('passenger-name');
+const ticketType = document.getElementById('ticket-offer');
+const trainPosition = document.getElementById('carriage');
+const ticketCode = document.getElementById('ticket-code');
+const ticketPrice = document.getElementById('ticket-price');
+const container = document.getElementById('micro');
+
+// Scrivendo nel form prendiamo i valori dell'input e li scriviamo nei determinati blocchi
+
+container.addEventListener('input' , (event) => {
+
+
+    // Blocchiamo l'invio del form
+    event.preventDefault();
+
+    // Stampiamo il codice
+    
+
+
+
+})
+
