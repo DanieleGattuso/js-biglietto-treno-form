620db07339cca8b40ee48658fb23f97725c6cd65
daniele.gattuso <d.gattuso09@gmail.com>
2025-01-13 17:25:31 +0100

    50% working excercize

--- js/script.js
+++ js/script.js
@@ -5,9 +5,12 @@ const nameField = document.getElementById('nome_biglietto');
 const kmField = document.getElementById('km_biglietto');
 const ageField = document.getElementById('age_biglietto');
 
+
+
+
 // Definiamo tutti gli elementi della pagina in output
 
-const name = document.getElementById('passenger-name');
+const passengerName = document.getElementById('passenger-name');
 const ticketType = document.getElementById('ticket-offer');
 const trainPosition = document.getElementById('carriage');
 const ticketCode = document.getElementById('ticket-code');
@@ -20,10 +23,12 @@ container.addEventListener('input' , (event) => {
 
 
     // Blocchiamo l'invio del form
-    event.preventDefault();
+    // event.preventDefault();
 
     // Stampiamo il codice
-    
+
+    passengerName.textContent = nameField.value.trim();
+
 
 
 
